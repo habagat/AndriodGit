ecfde6924c90c7f80fd8e5a20e45bd5f99073444
habagat <elettersko@yahoo.com>
2017-08-07 11:22:07 +0800

    Fragments
    Using Fragments Chapter 3

---
+++ Fragments/app/src/main/java/com/kunyari/fragments/Fragment1.java
@@ -0,0 +1,20 @@
+package com.kunyari.fragments;
+
+/**
+ * Created by wens on 8/7/2017.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class Fragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+//---Inflate the layout for this fragment---
+        return inflater.inflate(
+                R.layout.fragment1, container, false);
+    }
+}
